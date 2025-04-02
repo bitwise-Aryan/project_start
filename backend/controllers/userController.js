c9c9d57008888ccc3d148fc6acd02dcb30a9e58d
Shriyansh212 <shriyanshg54.com>
2025-04-03 00:18:46 +0530

    Updated userController.js and index.js, added middlewares

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import User from "../models/userModels.js";
-import asyncHandler from
+import asyncHandler from "../middlewares/asyncHandler.js";
 
 const createUser = asyncHandler(async (req, res) => {
     res.send("User created");
--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cors from "cors"; // Import cors
 
 //utils
 import connectDb from "./config/db.js"; 
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -23,8 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use("/api/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
 
 app.listen(port, () => console.log(`Server running on port:${port}`));
