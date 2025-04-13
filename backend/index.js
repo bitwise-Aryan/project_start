0324dac6f1f7d665f7f053bcc7e6b4552b57442b
Shriyansh212 <shriyanshg54.com>
2025-04-14 01:20:29 +0530

    Add product Controller and Routes

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,39 @@
-//packages
+// packages
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"; // Import cors
 
-//utils
-import connectDb from "./config/db.js"; 
+// Utiles
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
-import categoryRoutes from "./routes/categoryRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+//import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-connectDb();
+connectDB();
+
 const app = express();
 
-app.use(cors({
-    origin: "*", // Allows requests from any origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific methods
-    credentials: true // Allows cookies if needed
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
-app.use("/api/category",categoryRoutes)
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+// app.use("/api/upload", uploadRoutes);
+//app.use("/api/orders", orderRoutes);
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World");
+// app.get("/api/config/paypal", (req, res) => {
+//   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 // });
 
-app.listen(port, () => console.log(`Server running on port:${port}`));
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
