c3aad1fd7257322b1e3a04bf3fb3b54cb5f7c2c7
Shriyansh212 <shriyanshg54.com>
2025-04-16 23:29:15 +0530

    home page frontend

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,11 +3,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "./features/auth/authSlice.js";
+import favoriteReducer from "./features/favorites/favoriteSlice.js";
+import { getFavoritesFromLocalStorage } from "../Utils/localStorage.js";
 
+const initialFavorites = getFavoritesFromLocalStorage() || [];
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authReducer,
+        favorites: favoriteReducer,
+    },
+    preloadedState: {
+        favorites: initialFavorites,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
