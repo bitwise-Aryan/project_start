9af7a22a1831c72cff4e544a273ab1bc90ccbde2
Aryan Kumar <aryansingh19july@gmail.com>
2025-04-17 19:24:05 +0530

    frontend created and cncted for favorites,Ratings,Review,PrdctDescription

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -12,6 +12,7 @@ import "./Navigation.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../../redux/api/usersApiSlice";
 import { logout } from "../../redux/features/auth/authSlice";
+import FavoritesCount from "../Products/FavoritesCount";
 
 const Navigation = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -67,12 +68,14 @@ const Navigation = () => {
           <AiOutlineShoppingCart className="mr-2" size={26} />
           <span className="nav-item-name ml-2">CART</span>
         </Link>
-        <Link
-          to="/favorite"
-          className="flex items-center transition-transform transform hover:translate-x-2"
-        >
-          <FaHeart className="mr-2" size={20} />
-          <span className="nav-item-name ml-2">FAVORITES</span>
+        <Link to="/favorite" className="flex relative">
+          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+            <FaHeart className="mt-[3rem] mr-2" size={20} />
+            <span className="hidden nav-item-name mt-[3rem]">
+              Favorites
+            </span>{" "}
+            <FavoritesCount />
+          </div>
         </Link>
       </div>
 
